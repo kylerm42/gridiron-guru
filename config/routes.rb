51d3c8429bda446291cca4aa23256caa32cc3a6f
Kyle Moore <kylerm42@gmail.com>
2014-04-05 19:10:00 -0700

    created new branch for making a separate rails route for each league

--- config/routes.rb
+++ config/routes.rb
@@ -5,13 +5,13 @@ GridironGuru::Application.routes.draw do
     resource :session, only: :index
   end
 
+  resources :leagues, only: [:index, :show, :create, :update, :destroy]
+
   namespace :api, defaults: { format: :json } do
-    resources :leagues, only: [:index, :show, :create, :update, :destroy] do
-      resources :teams, only: [:index, :show, :create, :update, :destroy]
-      resources :add_drops, only: [:create]
-      resources :players, only: [:index, :show]
-      resources :trades, only: [:create, :update, :destroy]
-    end
+    resources :teams, only: [:index, :show, :create, :update, :destroy]
+    resources :add_drops, only: [:create]
+    resources :players, only: [:index, :show]
+    resources :trades, only: [:create, :update, :destroy]
     resources :roster_spots, only: [:index, :create, :update, :destroy]
     resources :matchups, only: [:show, :index]
   end
